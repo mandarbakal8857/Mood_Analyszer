bc25157fbedc7d8cc378da5e7e7efcd184c646c8
mandar <mandarbakal3@gmail.com>
2023-01-31 20:52:29 +0530

    Test Cases using Exception Handling

---
+++ MoodAnalyser.java
@@ -0,0 +1,37 @@
+package com.bridgelabs.modeanalyzer;
+
+import java.util.Scanner;
+
+public class MoodAnalyser
+{
+		public void analyseMood()
+		{
+			String mood1="happy";
+			String mood2="sad";
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter u r mood either happy or sad:");
+			String mood=sc.next();
+			try
+			{
+				
+			
+			if(mood.equals(mood1))
+			{
+				System.out.println("Happy Mood..");
+			}else if(mood.equals(mood2))
+			{
+				System.out.println("Sad Mood..");
+			}
+			}catch(NullPointerException e) 
+			{
+				System.out.println("Happy");
+			}
+		}
+		public static void main(String[] args) {
+			MoodAnalyser md=new MoodAnalyser();
+			md.analyseMood();
+		}
+
+	}
+
+
